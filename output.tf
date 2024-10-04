62ab720760651864d97d421421508c9556367934
rajsekhar <rajsekhar9655@gmail.com>
2024-10-04 11:21:11 +0530

    modify

--- output.tf
+++ output.tf
@@ -1,5 +1,19 @@
-/* output "def_vpc_info" {
-
-  value = data.aws_vpc.default_vpc
+output  "vpc_id" {
+  value       = aws_vpc.test_vpc.id #test_vpc
 }
-*/
+
+output "public_subnet_ids"{
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids"{
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids"{
+  value = aws_subnet.database[*].id
+}
+
+output "database_subnet_group_name"{
+  value = aws_db_subnet_group.default.name
+}
